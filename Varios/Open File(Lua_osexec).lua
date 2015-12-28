4d3b83562d692f606f3d84754e8c2418ae6d7165
EUGEN27771 <pops27771@gmail.com>
2015-12-29 01:32:07 +0200

    Update Open File(Lua_osexec).lua

--- Varios/Open File(Lua_osexec).lua
+++ Varios/Open File(Lua_osexec).lua
@@ -1,8 +1,9 @@
 ---Specify the full path to the file in double square brackets---
-File_Patch = [[C:\Users\EUGEN\Desktop\Example.txt]]
+File_Patch = [[D:\Probe.txt]]
 
 --Open File-----------------------------------------
 if File_Patch then
    File_Patch = string.gsub(File_Patch, [[\]], [[/]])
-   os.execute(File_Patch)
+   Command = "start "..File_Patch
+   os.execute(Command)
 end
