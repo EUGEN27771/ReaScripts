4c5c04cf1739bb50c3d704d4c714012777722078
EUGEN27771 <pops27771@gmail.com>
2016-05-19 20:02:57 +0300

    Delete Open File(BR_WinAPI).lua

--- Varios/Open File(BR_WinAPI).lua
+++
@@ -1,6 +0,0 @@
---Set Dir and File name--
-Dir = [[D:\]]--Set directory
-File = [[Probe.txt]]--Set File Name
-------------------------------------------------
-ShowFlag=3; Par=0
-Ret=reaper.BR_Win32_ShellExecute("open", File, Par, Dir, ShowFlag)
